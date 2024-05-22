d4059d19050a4fc97d47e0fc7c29a81fc54049b7
jesusvs1100 <jesusvs1100@gmail.com>
2024-05-22 15:58:42 -0600

    act 2 init project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Scanner
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Variables
+        String studentName;
+        float[] studentGrades = new float[5];
+        float average;
+        String studentStatus;
+
+        // Inputs
+        System.out.println("Ingresa el nombre del estudiante: ");
+        studentName = scanner.nextLine();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingresa la calificación numero " + (i + 1) + " del estudiante: ");
+            studentGrades[i] = scanner.nextFloat();
+        }
+
+        // Close scanner
+
+        scanner.close();
+
+        // Process
+
+        average = calculateAverage(studentGrades);
+        studentStatus = getStudentStatus(average);
+
+        // Outputs
+
+        System.out.println("Nombre del estudiante: " + studentName);
+        System.out.println("Calificaciones: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Calificación " + (i + 1) + ": " + studentGrades[i]);
+        }
+        System.out.println("Promedio: " + average);
+        System.out.println("Calificación: " + studentStatus);
+
+    }
+    public static float calculateAverage(float[] studentGrades) {
+        float sum = 0;
+        for (float studentGrade : studentGrades) {
+            sum += studentGrade;
+        }
+        return sum / studentGrades.length;
+    }
+    public static String getStudentStatus(float average) {
+        if (average >= 91) {
+            return "A";
+        } else if (average >= 81 && average <= 90) {
+            return "B";
+        } else if (average >= 71 && average <= 80) {
+            return "C";
+        } else if (average >= 61 && average <= 70) {
+            return "D";
+        } else if (average >= 51 && average <= 60) {
+            return "E";
+        } else {
+            return "F";
         }
     }
 }
